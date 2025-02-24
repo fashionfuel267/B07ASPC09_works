a6feaad8ce267f75921524add1423e7fa41fb0cd
Shahanaj <fashionfuel267@gmail.com>
2025-02-24 16:21:30 +0600

    Add project files.

---
+++ B07ASPC09_works/Models/DiagonosticContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace B07ASPC09_works.Models
+{
+    public class DiagonosticContext:DbContext
+    {
+        public DiagonosticContext(DbContextOptions<DiagonosticContext>options):base(options)
+        {
+            
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+    }
+   public enum Gender
+    {
+        Male=1,
+        Female,
+        Other
+    }
+    public class Patient
+    {
+        public int Id { get; set; }
+        [StringLength(15)]
+       
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public Gender PatientGender { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ B07ASPC09_works/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace B07ASPC09_works.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
